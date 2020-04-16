95cec4cb79043f518794a629ee4a32c04199c0eb
Ved <vedraj360@gmail.com>
2020-04-16 19:52:47 +0530

    Updates

---
+++ precache-manifest.e0bb648b87fe6bb136b77d07e6955e1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5899328b66bd26b83c10a5b17caa59",
+    "url": "/instavideodownloader/index.html"
+  },
+  {
+    "revision": "c549a59a07fefbcffddf",
+    "url": "/instavideodownloader/static/css/main.0e02aef1.chunk.css"
+  },
+  {
+    "revision": "ca71e4b753d210df85cc",
+    "url": "/instavideodownloader/static/js/2.cfcaff8b.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/instavideodownloader/static/js/2.cfcaff8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c549a59a07fefbcffddf",
+    "url": "/instavideodownloader/static/js/main.81b7ce7d.chunk.js"
+  },
+  {
+    "revision": "8afa726771d0fecffa8b",
+    "url": "/instavideodownloader/static/js/runtime-main.55e4a370.js"
+  }
+]);
